83b4b03a12783548b6ecae03f337a432010634bc
hruden <119438895+hruden@users.noreply.github.com>
2023-05-29 14:53:35 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -125,7 +125,7 @@ function handleInfiniteScroll() {
   }
 }
 
-//GO TO TOP BUTTON
+// /GO TO TOP BUTTON
 let mybutton = document.getElementById('myBtn');
 window.onscroll = function () {
   scrollFunction();
